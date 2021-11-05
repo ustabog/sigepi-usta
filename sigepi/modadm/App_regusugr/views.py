2b0dcfe839676d22b5ecb3ad8e49094558ec0761
JuanSebastianCely <92339618+JuanSebastianCely@users.noreply.github.com>
2021-11-05 00:20:23 -0500

    5/11/12: Modificación plantillas

--- sigepi/modadm/App_regusugr/views.py
+++ sigepi/modadm/App_regusugr/views.py
@@ -3,15 +3,37 @@ from django.views.generic import CreateView, DeleteView,ListView,UpdateView
 from django.urls import reverse_lazy
 from .form import *
 from modadm.App_regusugr.models import usugr
-from modcons.App_cons.form import frm_con_usugr
-from modcons.App_cons.views import vts_ls_usugr
+#from modcons.App_cons.form import frm_con_usugr
+#from modcons.App_cons.views import vts_ls_usugr 
+from django.http import HttpResponse
+
 
 class vts_reg_usugr(CreateView):
     #Clase que devuelve un formulario para registro de usuario grupo
     form_class = frm_con_usugr
-    template_name = 'nvo_usugr_prb.html'
+    template_name = 'App_regusugr_frm_creargrupo.html' 
     success_url = reverse_lazy('cons_usugr')
     success_message = "El usuario fue creado correctamente"
+
+###############Vista modulo de consulta##################
+class vts_ls_usugr(ListView):
+    # clase que me lsta todos los grupos
+    model = usugr
+    form_class = frm_con_usugr
+    template_name = 'cn_usugr.html'
+    success_url = reverse_lazy('cn_usugr.html')
+    success_message = 'listado cargado correctamente'
+#########################################################
+
+#Se trae de Consultas
+class vts_ls_usugr(ListView):
+    # clase que me lsta todos los grupos
+    model = usugr
+    form_class = frm_con_usugr
+    template_name = 'cn_usugr.html'
+    success_url = reverse_lazy('cn_usugr.html')
+    success_message = 'listado cargado correctamente'
+
 
 class vts_selc_usugr_mod(vts_ls_usugr):
     # clase para heredar la consulta de grupo
