a3ee50f196b14792a298f60949294786d05caf2d
Milton Castro <miltoncastro@usantotomas.edu.co>
2022-06-02 20:14:49 -0500

    versión de prueba 02-06-2022

--- modpry/App_regpry/urls.py
+++ modpry/App_regpry/urls.py
@@ -7,7 +7,7 @@ from .models import *
 urlpatterns = [ 
     
     #URL para registro de proyecto individual
-    path('inicio',vst_pry().vst_inicio, name = 'inicio'), #inicio de la app de proyectos
+    path('inicio',vst_pry().vst_inicio, name = 'inicio_pry'), #inicio de la app de proyectos
     path('creapry/',vts_reg_pry.as_view(), name = 'crea_pry'), #Crea el proyecto 
     path('cons_pry/', vts_ls_pry.as_view(), name='cn_pry'), #Lista de proyectos
     path('editpry/<int:pk>/',vts_edit_pry.as_view(), name = 'edit_pry'), #Actualizar proyecto
