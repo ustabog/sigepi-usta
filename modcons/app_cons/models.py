741b086438d92ea97e77ef4396993ff471fad969
LauraS189 <laurasrodriguez@usantotomas.edu.co>
2022-06-28 11:38:42 -0500

    Actualización modelos en apps_cons

--- modcons/app_cons/models.py
+++ modcons/app_cons/models.py
@@ -14,41 +14,6 @@ UNIDAD_MED_TIEM = [
     (3, 'Meses'),
     (4, 'Años')
     ]
-DEPARTAMENTOS = [
-    (0,'Antioquia'),
-    (1,'Atlántico'),
-    (2,'Abriaquí'),
-    (3,'Bogotá D.C'),
-    (4,'Boyacá'),
-    (5,'Caldas'),
-    (6,'Caquetá'),
-    (7,'Cauca'),
-    (8,'Cesar'),
-    (9,'Córdoba'),
-    (10,'Cundinamarca'),
-    (11,'Chocó'),
-    (12,'Huila'),
-    (13,'La guajira'),
-    (14,'Magdalena'),
-    (15,'Meta'),
-    (16,'Ñariño'),
-    (17,'Norte de Santander'),
-    (18,'Quindio'),
-    (19,'Risaralda'),
-    (20,'Santander'),
-    (21,'Sucre'),
-    (22,'Tolima'),
-    (23,'Valle del Cauca'),
-    (24,'Arauca'),
-    (25,'Casanare'),
-    (26,'Putumayo'),
-    (27,'Archipiélago de San Andrés, Providencia y Santa Catalina'),
-    (28,'Amazonas'),
-    (29,'Guainía'),
-    (30,'Guaviare'),
-    (31,'Vaupés'),
-    (32,'Vichada'),
-    ]
 
 #---------------------------------------------DTO-----------------------------------------
 #*************Para el modulo de proyectos*******************
